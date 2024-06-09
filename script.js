3f509030b34cec05afa71868ca8285c5805939c9
melaniedeiven <65181904+melaniedeiven@users.noreply.github.com>
2024-06-08 20:02:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+
+    navbarToggler.addEventListener('click', function () {
+        if (navbarCollapse.classList.contains('show')) {
+            navbarCollapse.classList.remove('show');
+        } else {
+            navbarCollapse.classList.add('show');
+        }
+    });
+});
